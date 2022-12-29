1f2970c67f3dfc6b3655bbb9884b3e8664c8ccb2
pelthepu <pavan.elthepu@gmail.com>
2022-12-29 14:52:38 +0530

    Initial Commit

--- src/main/java/com/pavan/utility/UtilityApplication.java
+++ src/main/java/com/pavan/utility/UtilityApplication.java
@@ -1,13 +1,13 @@
-package com.pavan.todo;
+package com.pavan.utility;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class UtilityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
+		SpringApplication.run(UtilityApplication.class, args);
 	}
 
 }
